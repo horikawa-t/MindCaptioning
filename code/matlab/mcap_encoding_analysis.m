8187faf84d56849c58d4dbf9bc2b561d44dc5800
Tomoyasu Horikawa <horikawa-t@atr.jp>
2024-05-20 16:51:11 +0900

    Add files via upload

--- code/matlab/mcap_encoding_analysis.m
+++ code/matlab/mcap_encoding_analysis.m
@@ -10,23 +10,13 @@
 %% initialize
 clear all, close all
 
-% general info.
-cd('/home/psi/horikawa-t/toolbox/public/mcap/');
+% set path to MindCaptioning directory
 p.rootPath = './';
 p.analysisType = 'encoding';
 addpath(genpath([p.rootPath,'code/']));
 
 %% set paramters
 p = mcap_setParams(p);
-
-% overwrite for demo (relatively faster)
-do_demo = 1; % set 0, if you want to completely reproduce the manuscript results 
-if do_demo
-    %[4,4,5] can be used for demo [faster almost equivalent to our results]
-    p.aparam.l2.nparamLogSearch = 4;
-    p.aparam.l2.lowL = 4;
-    p.aparam.l2.highL = 5;
-end
 
 %% perform cross-validation encoding analyses
 mcap_cv_encoding(p)
---
+++ code/matlab/mcap_summary_decoding.m
@@ -0,0 +1,34 @@
+% function mcap_summary_decoding(p)
+%
+% This script is written to summarize decoding results in
+%  Horikawa, T. (2024) Mind captioning: Evolving descriptive text of mental
+%  content from human brain activity. bioRxiv. 
+%
+% written by Tomoyasu Horikawa horikawa.t@gmail.com 2024/05/13
+%
+%% initialize
+clear all, close all
+
+% set path to MindCaptioning directory
+p.rootPath = './';
+addpath(genpath([p.rootPath,'code/']));
+
+%% set paramters for decoding(text_generation) summary
+p.analysisType = 'text_generation';
+p = mcap_setParams(p);
+
+%% parameter settings
+p.mlmType = 'roberta-large';
+p.lmType = 'deberta-large';
+p.roiTypes = p.rparam.roiTypes;
+p.dataTypes = {'testPerception','testImagery'};
+
+%% load text generation results
+res = mcap_summary_decoding_summarize(p);
+
+%% draw figures for text generation results
+mcap_summary_decoding_draw_generatedtext(res,p);
+mcap_summary_decoding_draw_similarity(res,p);
+mcap_summary_decoding_draw_idenacc(res,p);
+
+%%
